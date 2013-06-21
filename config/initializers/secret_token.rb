90465dabddc1a9450b544f4972ad4a8128c43d2a
Strand McCutchen <strand@nird.us>
2013-06-21 13:48:41 -0700

    New Rails app super quick

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QuickApp::Application.config.secret_token = '9a735f371cc10d9125b55b8fbc9c1a3c609c619cef83ad2255d384f46dc32b61d88f83016dfe3e4bd290470343c40bce07be2c03da4596ce59e8f98d549c031b'
